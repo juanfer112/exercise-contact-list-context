d86919ea78c43200a5f78039f091c3a9c1af1fa7
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-06-01 15:14:01 +0000

    primera etapa fetch method Put

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,12 +2,44 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contact: {
+				full_name: "Pepe grillo",
+				email: "dave@gmail.com",
+				agenda_slug: "Pollan_agenda",
+				address: "47568 NW 34ST, 33434 FL, USA",
+				phone: "7864445566"
+			}
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			pullAgenda: async () => {
+				const response = await fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Pollan_agenda");
+				const data = await response.json();
+				console.log(data);
+			},
+			putAgenda: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/899", {
+					method: "PUT",
+					headers: {
+						"content-type": "application/json"
+					},
+					body: JSON.stringify(getStore().contact)
+				})
+					.then(response => {
+						console.log(response);
+					})
+					.catch(err => {
+						console.log(err);
+					});
+			}
 		}
 	};
 };
 
 export default getState;
+/*address: "Pontevedra Vigo, SPAIN"
+agenda_slug: "Pollan_agenda"
+created_at: "2020-06-01 11:32:14"
+email: "juanfer@gmail.com"
+full_name: "Juan Pollan"
+id: "899"
+phone: "555555555"*/
